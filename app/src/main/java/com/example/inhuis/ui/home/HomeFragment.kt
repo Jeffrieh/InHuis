5ca0a0422ccfa177f973f80f03a32c77b45c5f31
Jeffrey <jeffrey@1nteger.nl>
2020-05-27 13:16:04 +0200

    added FAB and dialog

--- app/src/main/java/com/example/inhuis/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/inhuis/ui/home/HomeFragment.kt
@@ -9,6 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.inhuis.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
 
 class HomeFragment : Fragment() {
 
@@ -19,13 +23,27 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
+
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+        val fab: View = root.findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
+
         return root
     }
+
+
+
+
+
+
 }
