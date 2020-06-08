7051ed5b23e377298373f394644bd66b7ec8345b
Jeffrey <jeffrey@1nteger.nl>
2020-06-08 12:14:15 +0200

    possible fix for drawable error

--- app/src/main/java/com/example/inhuis/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/example/inhuis/ui/home/HomeAdapter.kt
@@ -30,9 +30,14 @@ class HomeAdapter(private var myDataset: List<Ingredient>, private val context: 
 
     inner class HomeAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(item: Ingredient) = with(itemView) {
-            itemView.tvName.text = item.name;
-            itemView.imageView.setImageDrawable(context.getDrawable(item.image))
-            itemView.tvAmount.text = " - " + item.amount.toString() + "g";
+            try{
+                itemView.tvName.text = item.name;
+                itemView.imageView.setImageDrawable(context.getDrawable(item.image))
+                itemView.tvAmount.text = " - " + item.amount.toString() + "g";
+            }catch(e : Exception){
+                itemView.imageView.setImageDrawable(context.getDrawable(R.drawable.ic_notifications_black_24dp))
+                println(e.message);
+            }
         }
 
         public fun getIngredientAt(position: Int): Ingredient {
